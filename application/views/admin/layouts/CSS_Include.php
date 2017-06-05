f29538f548898d1d6e7efe57dc085822d08a59e7
Saket Anand <saket@traveliciousholiday.in>
2017-06-05 17:52:03 +0530

    Blank News Body

--- application/views/admin/layouts/CSS_Include.php
+++ application/views/admin/layouts/CSS_Include.php
@@ -14,3 +14,11 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css">
 
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.css">
+
+
+
+
+<!-- Head JS -->
+
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
