0d6c062f7bcd89b4e61e2c52db77cf74caa41043
ithgourav123 <gourav@traveliciousholiday.in>
2017-06-22 15:40:38 +0530

    d

--- application/views/admin/photos/add.php
+++ application/views/admin/photos/add.php
@@ -38,7 +38,6 @@
 
 </form>
 
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/vendor/jquery/jquery-1.12.3.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 
