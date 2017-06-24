a6abef9d33373c1d4977071ab25d4235ccd17cfe
Shailendra Katiyar <shailendra@traveliciousholiday.in>
2017-06-24 15:29:48 +0530

    add hot deals of a day

--- application/controllers/frontend/Home.php
+++ application/controllers/frontend/Home.php
@@ -24,6 +24,98 @@ class Home extends FrontendController {
 		$this->load->view('frontend/index');
 
 	}
+	
+	
+	
+	public function categories()
+	{
+	  $categories  = $this->db->select('*')          
+                              ->get('category');
+      $categories = $categories->result(); 							  
+	  if(!empty($categories))
+	  {
+		$data['categories'] = $categories; 
+        $this->load->view('frontend/categories', $data);		
+	  }
+	}
+	
+	
+	
+	public function getPackage()
+	{
+	  $categoryId = $this->input->post('category');
+	  if(is_numeric($categoryId) && $categoryId > 0)
+	  {
+		$results = $this->db->select('package_id')
+                            ->where('category_id', $categoryId)
+                            ->get('category_mapping');				 
+							
+	    $results = $results->result();
+		//print_r($results);
+        if(!empty($results))
+		{
+		  foreach($results as $result)
+		  {
+		    $results1 = $this->db->select('*')
+                                 ->where('id', $result->package_id)
+					             ->get('packages');
+            $results1 = $results1->result(); 							
+            if(!empty($results1))
+			{
+			  $data['packages'] = $results1;	
+			  $this->load->view('frontend/categories', $data);
+			}				
+		  }					 
+		}			
+	  }
+  	  
+	}
+	
+	
+	public function hotDealsOfADay()
+	{
+	  $id = $this->db->select('id')
+	                 ->where('category_name', 'Hot deals of a day')
+                     ->get('category');
+      $id = $id->result(); 
+	  if(!empty($id) && count($id) == 1)
+	  {		  
+		  foreach($id as $id1)
+		  {
+			$id = $id1->id;  
+		  }
+		  $packageIds = $this->db->select('package_id')
+								 ->where('category_id', $id)
+								 ->limit(6)
+								 ->get('category_mapping');
+          $packageIds = $packageIds->result(); 
+		  $data = array();
+		  $i=0;
+		  if(!empty($packageIds))
+		  {
+			  foreach($packageIds as $packageId)
+			  {
+				$packages = $this->db->select('*')
+									 ->where('id', $packageId->package_id)
+									 ->get('packages');
+				$packages = $packages->result();							 
+				if(!empty($packages))
+				{
+				  $i++;
+				  $data['hotPackages'][$i] = $packages;
+				}
+		     }
+		  }		  
+	  }	  
+      
+	  /*foreach($data['hotPackages'] as $hotPackages)
+	  {
+		print_r($hotPackages);
+        echo "<br>";  		
+	  }*/
+  	  
+	  $this->load->view('frontend/hot-deals', $data);
+	}
 
 
 }
