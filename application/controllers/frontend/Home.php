894a66d75b584f6227f9590b133fac1aed2819d0
Aditya Kumar <aditya@travelicious.com>
2017-06-24 17:17:07 -0700

    frontend packages display

--- application/controllers/frontend/Home.php
+++ application/controllers/frontend/Home.php
@@ -1,29 +1,28 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends FrontendController {
 
-	/**
-	<!-- ____________________ Alamgir ________________ -->
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('frontend/index');
+    function __construct() {
+        parent::__construct();
+        $this->load->model('frontend/Home_model');
+    }
 
-	}
+    public function index() {
+        $this->data['dest_list'] = $this->Home_model->fetch_destination_list();
 
+        $this->load->view('frontend/index', $this->data);
+    }
+    
+    public function searched_package() {
+        
+       $dst_id =  $this->input->post('dest_name');
+       $this->data['package_list'] = $this->Home_model->fetch_package_by_destination($dst_id);
+//    echo '<pre>';   print_r($this->data['package_list']); exit;
+       $this->load->view('frontend/search-result', $this->data);
+        
+    }
 
+   
 }
